acf5032c972c5055fe7e68631cbcd825334923d4
easyAZ1K2009 <you@example.com>
2024-03-07 22:55:55 +0500

    xammayo tayyor

---
+++ app.js
@@ -0,0 +1,17 @@
+const jobs = [
+  {
+    id: 7,
+    company: "Photosnap",
+    logo: "./images/photosnap.svg",
+    new: true,
+    featured: true,
+    position: "Senior Frondet Developer",
+    role: "Frondet",
+    level: "Senior",
+    postedAt: "1d ago",
+    contract: "Full Time",
+    location: "USA Only",
+    languages: ["HTML", "CSS", "JavaScript"],
+    tools: [],
+  },
+];
